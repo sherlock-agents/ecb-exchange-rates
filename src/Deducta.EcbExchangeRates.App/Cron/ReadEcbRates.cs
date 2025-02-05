3c7fdaec74bfa7e685b6f9a63eef63615702c1ad
Ian Kok <hello@iankok.com>
2025-02-05 14:08:45 +0100

    redeploy

--- src/Deducta.EcbExchangeRates.App/Cron/ReadEcbRates.cs
+++ src/Deducta.EcbExchangeRates.App/Cron/ReadEcbRates.cs
@@ -8,6 +8,7 @@ public class ReadEcbRates(IExchangeRateRepository exchangeRateRepository)
     [Function(nameof(ReadAndStoreExchangeRates))]
     public async Task ReadAndStoreExchangeRates([TimerTrigger("0 0 0 * * *")] TimerInfo timer, FunctionContext context)
     {
+        
         var rates = await exchangeRateRepository.GetExchangeRatesFromRemote();
         await exchangeRateRepository.StoreExchangeRates([rates]);
     }
